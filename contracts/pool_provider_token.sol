9f2506244c490286bf225317f3cab5458f70b3ac
Ad450 <adjeimanue@gmail.com>
2022-08-02 09:36:04 +0000

    pool provider token

--- contracts/pool_provider_token.sol
+++ contracts/pool_provider_token.sol
@@ -1,12 +1,12 @@
 pragma solidity ^0.8.7;
 // SPDX-License-Identifier: MIT
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PoolProviderTokens is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    function mintProviderTokens(address provider, uint256 _amount) internal {
+    function mintProviderTokens(address provider, uint256 _amount) external {
         _mint(provider, _amount);
     }
 }
