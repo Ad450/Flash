fda531b10afe8da3ee523e3b787f5b46af640059
Ad450 <adjeimanue@gmail.com>
2022-08-02 00:07:28 +0000

    more functionalities added

--- contracts/pool.sol
+++ contracts/pool.sol
@@ -1,11 +1,14 @@
 pragma solidity ^0.8.7;
 // SPDX-License-Identifier: MIT
-import "./interfaces/IPool.sol";
+import "./IPool.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+import "./pool_provider_tokens.sol";
 
 contract Pool is IPool {
-    bool locked;
+    mapping(address => bool) provider;
+    PoolProviderTokens private _providerTokens;
 
+    bool locked;
     modifier guard() {
         require(!locked, "function locked");
         locked = true;
@@ -20,15 +23,29 @@ contract Pool is IPool {
         guard
     {
         // check balance of caller
+        require(msg.sender != address(0), "invalid address");
+        require(_amount <= 0, "invalid amount");
         require(
             IERC20(_token).balanceOf(msg.sender) >= _amount,
             "insufficient balance"
         );
 
+        uint256 _initialPoolBalance = IERC20(_token).balanceOf(
+            address(_providerTokens)
+        );
         // transfer tokens to pool
         IERC20(_token).transfer(address(this), _amount);
-
         // send pool provider tokens to caller or sender
+        require(
+            IERC20(_token).balanceOf(address(_providerTokens)) >
+                _initialPoolBalance,
+            "invalid transfer"
+        );
+        provider[msg.sender] = true;
+        _providerTokens = new PoolProviderTokens("Pool Provider Tokens", "PPT");
+        // will calculate the amount of PPT to send to caller
+        // just using all _amount for now
+        _providerTokens.mintProviderTokens(msg.sender, _amount);
     }
 
     function withdrawTokens(address _token, uint256 _amount)
@@ -36,4 +53,6 @@ contract Pool is IPool {
         override
         guard
     {}
+
+    function _calculatePPT(address token, uint256 _amount) private {}
 }
