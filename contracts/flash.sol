a98c8a71b9200f106351381de1062557effcd5dc
Ad450 <adjeimanue@gmail.com>
2022-08-02 10:19:31 +0000

    initital phase of flash lend

--- contracts/flash.sol
+++ contracts/flash.sol
@@ -1,9 +1,18 @@
 pragma solidity ^0.8.7;
 //SPDX-License-Identifier:MIT
 import "./interfaces/IFlash.sol";
+import "./pool.sol";
 
 contract Flash is IFlash {
-    function lend(address token, uint256 _amount) external override {}
+    Pool private _pool;
+
+    constructor() {
+        _pool = new Pool();
+    }
+
+    function lend(address token, uint256 _amount) external override {
+        _pool.addTokens(token, _amount);
+    }
 
     function borrow(address token, uint256 _amount) external override {}
 }
