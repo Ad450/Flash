b8008eef8cbf4ba4cfde8cb65d8bd555d8dfb28b
Ad450 <adjeimanue@gmail.com>
2022-08-06 15:54:04 +0000

    dai to eth price feeds from chainlink

---
+++ contracts/price_feeds.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceFeeds {
+    AggregatorV3Interface private priceFeed;
+    // Aggregator is DAI/ETH
+    address private immutable rinkebyTestnet =
+        0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(rinkebyTestnet);
+    }
+
+    function getPrice() public view returns (int256 price) {
+        (
+            ,
+            /*uint80 roundID*/
+            price,
+            /*uint startedAt*/
+            /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            ,
+            ,
+
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
