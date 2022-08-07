c4127feb3a197534751fa8ad750799b4d56e2598
Ad450 <adjeimanue@gmail.com>
2022-08-07 17:08:48 +0000

    more configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,29 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    compilers: [{ version: "0.8.7" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    // ganache: {
+    //   url: process.env.GANACHE_URL,
+    //   accounts:
+    //     process.env.GANACHE_PRIVATE_KEY !== undefined
+    //       ? [process.env.GANACHE_PRIVATE_KEY]
+    //       : [],
+    // },
+  },
 };
 
 export default config;
