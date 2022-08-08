4f24e44c6e9c997839043281eaab67b1c056ef47
Ad450 <adjeimanue@gmail.com>
2022-08-08 01:11:41 +0000

    ganache configured

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -17,13 +20,13 @@ const config: HardhatUserConfig = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
-    // ganache: {
-    //   url: process.env.GANACHE_URL,
-    //   accounts:
-    //     process.env.GANACHE_PRIVATE_KEY !== undefined
-    //       ? [process.env.GANACHE_PRIVATE_KEY]
-    //       : [],
-    // },
+    ganache: {
+      url: process.env.GANACHE_URL,
+      accounts:
+        process.env.GANACHE_PRIVATE_KEY !== undefined
+          ? [process.env.GANACHE_PRIVATE_KEY]
+          : [],
+    },
   },
 };
 
