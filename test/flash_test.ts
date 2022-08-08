77f3222552ba09e2d9fb97bc2939bc51cc616f65
Ad450 <adjeimanue@gmail.com>
2022-08-08 01:12:00 +0000

    borrow reverting

--- test/flash_test.ts
+++ test/flash_test.ts
@@ -1,9 +1,14 @@
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
+import { Flash } from "../typechain-types";
 
 describe("Flash test", () => {
   let flash: Contract;
+  const DAI_ADDRESS: string = "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735";
+  const testDAIAmount: number = 50;
+  //ganache address
+  const SENDER_ADDRESS: string = "0x1B359b62CB683aB501a2Dd8bF92310BFB6c824c6";
 
   it("should deploy contract", async () => {
     const FlashFactory = await ethers.getContractFactory("Flash");
@@ -14,4 +19,15 @@ describe("Flash test", () => {
   });
 
   // borrow
+  it("should borrow", async () => {
+    const result = await flash.borrow(
+      DAI_ADDRESS,
+      ethers.utils.parseUnits("50"),
+      {
+        from: SENDER_ADDRESS,
+        value: ethers.utils.parseEther("1000000000000000000"),
+      }
+    );
+    console.log(result);
+  });
 });
