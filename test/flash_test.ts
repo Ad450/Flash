91b4dcf50c9f3a5ae7f07059bb82544d26ee6640
Ad450 <adjeimanue@gmail.com>
2022-08-15 23:33:48 +0000

    minor changes

--- test/flash_test.ts
+++ test/flash_test.ts
@@ -7,14 +7,16 @@ describe("Flash test", () => {
   let flash: Contract;
   const DAI_ADDRESS: string = "0xc7AD46e0b8a400Bb3C915120d284AafbA8fc4735";
   const testDAIAmount: number = 50;
-  //ganache address
-  const SENDER_ADDRESS: string = "0x1B359b62CB683aB501a2Dd8bF92310BFB6c824c6";
+  //ganache address - replace with ganache address
+  const SENDER_ADDRESS: string = "0x2f88198d42A2eab5C02813b33D206434A083CB22";
 
-  it("should deploy contract", async () => {
+  beforeEach(async () => {
     const FlashFactory = await ethers.getContractFactory("Flash");
     flash = await FlashFactory.deploy();
     await flash.deployed();
+  });
 
+  it("should deploy contract", async () => {
     expect(flash.address).is.not.null;
   });
 
@@ -22,11 +24,11 @@ describe("Flash test", () => {
   it("should borrow", async () => {
     const result = await flash.borrow(
       DAI_ADDRESS,
-      ethers.utils.parseUnits("50"),
-      {
-        from: SENDER_ADDRESS,
-        value: ethers.utils.parseEther("1000000000000000000"),
-      }
+      ethers.utils.parseUnits("50")
+      // {
+      //   from: SENDER_ADDRESS,
+      //   value: ethers.utils.parseEther("1000000000000000000"),
+      // }
     );
     console.log(result);
   });
